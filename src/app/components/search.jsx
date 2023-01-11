d9b618b538975c189f12958be5aec9673043ac0e
SergeyMaxx <maximov.regen@gmail.com>
2023-01-11 16:09:45 +0300

    доработка стилей иконок и страницы заметки

--- src/app/components/search.jsx
+++ src/app/components/search.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types'
 const Search = ({setSearchText}) => {
   return (
     <div className="note-list__search">
-      <i className="note-list__search_icon"/>
       <input
         type="text"
         className="note-list__search_input"
         onChange={e => setSearchText(e.target.value)}
         placeholder="Search..."
       />
+      <i className="note-list__search_icon"/>
     </div>
   )
 }
