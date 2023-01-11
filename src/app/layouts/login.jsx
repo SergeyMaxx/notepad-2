1e95124e90bcb2096b3fb643bb9efeb9ab0537ad
SergeyMaxx <maximov.regen@gmail.com>
2023-01-11 15:21:39 +0300

    реализована валидация

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,27 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useHistory} from 'react-router-dom'
 
 const Login = () => {
   const history = useHistory()
+  const [data, setData] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = ({target}) => {
+    setData(prevState => ({
+      ...prevState,
+      [target.name]: target.value
+    }))
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(data)
+  }
 
   return (
     <div className="login">
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
         <h1 className="login__header">Login</h1>
         <label>
           <input
+            name="email"
             type="text"
+            value={data.email}
+            required
+            onChange={handleChange}
             placeholder="Email Address"
             className="login__input"
-            required
           />
         </label>
         <label>
           <input
+            name="password"
             type="password"
+            value={data.password}
+            required
+            onChange={handleChange}
             placeholder="Password"
             className="login__input password"
-            required
           />
         </label>
         <p className="login__account">
--- src/app/layouts/register.jsx
+++ src/app/layouts/register.jsx
@@ -1,35 +1,104 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useHistory} from 'react-router-dom'
+import {validator} from '../utils/validator'
 
 const Register = () => {
   const history = useHistory()
+  const [errors, setErrors] = useState({})
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+    name: ''
+  })
+
+  const validatorConfig = {
+    email: {
+      isEmail: {message: 'Email entered incorrectly'}
+    },
+    name: {
+      min: {
+        message: 'Name must contain at least 2 characters',
+        value: 2
+      }
+    },
+    password: {
+      isCapitalSymbol: {message: 'Password must contain a capital letter'},
+      isContainDigit: {message: 'Password must contain a number'},
+      min: {
+        message: 'Password must contain at least 8 characters',
+        value: 8
+      }
+    }
+  }
+
+  const handleChange = target => {
+    setData(prevState => ({
+      ...prevState,
+      [target.name]: target.value
+    }))
+  }
+
+  const onChange = ({target}) => {
+    handleChange({name: target.name, value: target.value})
+  }
+
+  // useEffect(() => {
+  //   validate()
+  // }, [data])
+
+  const validate = () => {
+    const errors = validator(data, validatorConfig)
+    setErrors(errors)
+    return Object.keys(errors).length !== 0
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (validate()) return
+    console.log(data)
+  }
 
   return (
     <div className="login">
-      <form onSubmit={e => e.preventDefault()}>
+      <form onSubmit={handleSubmit}>
         <h1 className="login__header register__header">Registration</h1>
+        <p className="errors register__errors-name">{errors.name}</p>
         <label>
           <input
+            name="name"
+            id="name"
             type="text"
-            placeholder="Name"
-            className="login__input"
+            value={data.name}
             required
+            onChange={onChange}
+            placeholder="Name"
+            className="login__input name-register"
           />
         </label>
+        <p className="errors register__errors-email">{errors.email}</p>
         <label>
           <input
+            name="email"
+            id="email"
             type="text"
+            value={data.email}
+            required="required"
+            onChange={onChange}
             placeholder="Email Address"
             className="login__input email"
-            required
           />
         </label>
+        <p className="errors register__errors-password">{errors.password}</p>
         <label>
           <input
+            name="password"
+            id="password"
             type="password"
+            value={data.password}
+            required="required"
+            onChange={onChange}
             placeholder="Password"
             className="login__input password password-register"
-            required
           />
         </label>
         <p className="login__account already">
